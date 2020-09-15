189fd5e8c008a9b02e31624d37a6ad6459ad673d
Pin Huang <justmailmason@gmail.com>
2020-09-15 19:25:03 +0800

    Initial Commit

---
+++ simplified_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simplified_calculator
+//
+//  Created by Pincheng Huang on 2020/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ simplified_calculatorTests/simplified_calculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  simplified_calculatorTests.swift
+//  simplified_calculatorTests
+//
+//  Created by Pincheng Huang on 2020/9/15.
+//
+
+import XCTest
+@testable import simplified_calculator
+
+class simplified_calculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
